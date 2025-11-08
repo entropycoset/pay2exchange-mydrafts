7a54ce08f7d2ef04ec093acb4f6cb901d8c8139a
entropycoset <nullcoset@onet.pl>
2025-11-08 05:07:27 +0100

    target run - executes test

--- stdpipe/simple_test.sh
+++ stdpipe/simple_test.sh
@@ -7,7 +7,7 @@ echo -e "ping\nquit" | {
     # Create file descriptors 3 and 4 from stdin and stdout
     exec 3<&0  # fd 3 reads from stdin
     exec 4>&1  # fd 4 writes to stdout
-    ./build/stdpipe_serv 3 4
+    ./stdpipe_serv 3 4
 }
 
 echo "Test completed successfully!"
