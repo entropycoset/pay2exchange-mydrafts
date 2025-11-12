885668d09d352d975682cb6c8f9e5086da8f4330
entropycoset <nullcoset@onet.pl>
2025-11-12 09:18:09 +0100

    abstrsock build scr

--- examples/testipc/sockabstr/build.sh
+++ examples/testipc/sockabstr/build.sh
@@ -1,6 +1,6 @@
 # Compile server
-g++ -std=c++17 -Wall -O2 myapp.cpp -o myapp -lpthread
+g++ -std=c++17 -Wall -O2 server.cpp -o server -lpthread
 
 # Compile client
-g++ -std=c++17 -Wall -O2 tester.cpp -o tester -lpthread
+g++ -std=c++17 -Wall -O2 client.cpp -o client -lpthread
 
